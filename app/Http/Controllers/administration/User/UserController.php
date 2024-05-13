599c3d011f393678f760ee8b24ff9ae972828e6f
kharma <angelparedestorres.apt@gmail.com>
2024-05-12 19:38:44 -0600

    Refactor: Modificacines para convertir las funciones en endpoint para la API.

--- app/Http/Controllers/administration/User/UserController.php
+++ app/Http/Controllers/administration/User/UserController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers\administration\User;
 
 use App\Helpers\SharedFunctionsHelpers;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Gate;
 use Yajra\DataTables\DataTables;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use App\Models\User;
+use Carbon\Carbon;
 use Exception;
+use Illuminate\Http\Response;
+
+use function PHPUnit\Framework\isNull;
 
 class UserController extends Controller
 {
@@ -18,11 +24,6 @@ class UserController extends Controller
     public function __construct(SharedFunctionsHelpers $shared)
     {
         $this->shared = $shared;
-        $this->middleware('auth');
-        $this->middleware(function ($request, $next) {
-            $this->shared->getUserStatus();
-            return $next($request);
-        });
     }
 
     /*
@@ -31,41 +32,70 @@ class UserController extends Controller
     |----------------------------------------------------------------------------------------------------
     */
 
-    public function getAccount($uuid)
+    public function getAccount(Request $request)
     {
         # Politica para saber si el usuario cuenta con los permisos deacuerdo a su rol asignado
         // Gate::authorize('getAccount');
 
-        try {
-            $account = User::where('uuid', $uuid)->firstOrFail();
+        $validatedData = $request->validate([
+            'uuid' => 'required|string|uuid|exists:users,uuid',
+        ]);
 
-            return view('pages.account.user.account', compact('account'));
+        try {
+            $account = User::with('roles')
+                ->select('users.*')
+                ->where('uuid', $validatedData['uuid'])
+                ->firstOrFail();
+
+            $userData = [
+                'uuid' => $account->uuid,
+                'name' => $account->name,
+                'email' => $account->email,
+                'role' => optional($account->roles->first())->name ?? 'N/A',
+                'department' => $account->department,
+                'position' => $account->position,
+                'is_active' => $account->is_active,
+                'last_login' => $account->last_login,
+                'phone' => $account->phone,
+                'notes' => $account->notes,
+                'created_at' => date('d-m-Y H:i:s', strtotime($account->created_at)),
+            ];
+
+            return $this->shared->sendResponse($userData, $this->shared->getDataMessage(), Response::HTTP_OK);
         } catch (Exception $error) {
-            return redirect()->route('dashboard')->with('error', $error->getMessage());
+            return $this->shared->sendError($error->getMessage(), 'Error', Response::HTTP_BAD_REQUEST);
         }
     }
 
-    public function editAccount(Request $request, $uuid)
+    public function editAccount(Request $request)
     {
         # Politica para saber si el usuario cuenta con los permisos deacuerdo a su rol asignado
         // Gate::authorize('editAccount');
 
-        $this->validate($request, [
-            'name'  => 'nullable|min:3|max:255|regex:/^[\pL\s\-]+$/u',
-            'phone' => 'nullable|regex:/^\(\d{3}\) \d{3}-\d{4}$/',
-            'notes' => 'nullable|min:5|max:2147483647',
+        $validatedData = $request->validate([
+            'name'  => 'required|string|min:3|max:255|regex:/^[\pL\s\-]+$/u',
+            'phone' => 'required|regex:/^\(\d{3}\) \d{3}-\d{4}$/',
+            'notes' => 'required|min:5|max:2147483647',
+            'uuid' => 'required|string|uuid|exists:users,uuid',
         ]);
 
         try {
-            $account = User::where('uuid', $uuid)->firstOrFail();
-            $account->name = $request->name;
-            $account->phone = $request->phone;
-            $account->notes = $request->notes;
+            $account = User::where('uuid', $validatedData['uuid'])->firstOrFail();
+            $account->name = $this->shared->clearString($validatedData['name']);
+            $account->phone = $validatedData['phone'];
+            $account->notes = $validatedData['notes'];
+            $account->updated_at = Carbon::now();
             $account->save();
 
-            return $this->shared->sendResponse($account, 'Cuenta Actualizada con éxito!');
+            $userData = [
+                'name' => $account->name,
+                'phone' => $account->phone,
+                'notes' => $account->notes,
+            ];
+
+            return $this->shared->sendResponse($userData, 'Cuenta Actualizada con éxito!', Response::HTTP_OK);
         } catch (Exception $error) {
-            return $this->shared->sendError($error->getMessage(), 'Sucesio un problema intente');
+            return $this->shared->sendError($error->getMessage(), 'Sucesio un problema intente nuevamente.', Response::HTTP_BAD_REQUEST);
         }
     }
 
@@ -77,47 +107,31 @@ class UserController extends Controller
 
     public function listUser()
     {
+        $usersData[] = [];
         $users = User::with('roles')
             ->select('users.*')
-            ->orderByDesc('id');
+            ->orderByDesc('id')
+            ->where('deleted_at', null)
+            ->get();
 
-        $departments = [
-            ['id' => 1, 'name' => 'Ventas'],
-            ['id' => 2, 'name' => 'Recursos Humanos'],
-            ['id' => 3, 'name' => 'Sistemas'],
-            ['id' => 4, 'name' => 'Intendencia'],
-            ['id' => 5, 'name' => 'Dirección'],
-            ['id' => 6, 'name' => 'Otro']
-        ];
-
-        $positions = [
-            ['id' => 1, 'name' => 'Empleado'],
-            ['id' => 2, 'name' => 'Vendedor'],
-            ['id' => 3, 'name' => 'Limpieza'],
-            ['id' => 4, 'name' => 'Soporte Tecnico'],
-            ['id' => 5, 'name' => 'Director'],
-            ['id' => 6, 'name' => 'Cordinador']
-        ];
-
-        if (request()->ajax()) {
-            return datatables()->of($users)
-                ->addColumn('options', function ($user) {
-                    return view('pages.system.users.shared.options', ['uuid' => $user->uuid]);
-                })
-                ->addColumn('is_active', function ($user) {
-                    return $user->is_active ? '<span class="badge badge-success">Activo</span>' : '<span class="badge badge-danger">Inactivo</span>';
-                })
-                ->addColumn('role', function ($user) {
-                    return $user->roles->first()->name ?? 'N/A';
-                })
-                ->addColumn('created_at', function ($user) {
-                    return date('d-m-Y H:i:s', strtotime($user->created_at));
-                })
-                ->rawColumns(['options', 'is_active'])
-                ->make(true);
+        foreach ($users as $value) {
+            $userData[] = [
+                'uuid' => $value->uuid,
+                'name' => $value->name,
+                'email' => $value->email,
+                'department' => $value->department,
+                'position' => $value->position,
+                'is_active' => $value->is_active,
+                'phone' => $value->phone,
+                'notes' => $value->notes,
+                'last_login' => isNull($value->last_login) ? 'N/A' : date('d-m-Y H:i:s', strtotime($value->last_login)),
+                'role' => optional($value->roles->first())->name ?? 'N/A',
+                'created_at' => date('d-m-Y', strtotime($value->created_at)),
+                'updated_at' => date('d-m-Y', strtotime($value->updated_at))
+            ];
         }
 
-        return view('pages.system.users.list', compact('departments', 'positions'));
+        return $this->shared->sendResponse($userData, 'Catalogo de cuentas de usuarios!', Response::HTTP_OK);
     }
 
     public function createUser(Request $request, User $user)
@@ -125,7 +139,7 @@ class UserController extends Controller
         # Politica para saber si el usuario cuenta con los permisos deacuerdo a su rol asignado
         // Gate::authorize('createUser');
 
-        $this->validate($request, [
+        $validatedData = $request->validate([
             'name'  => 'required|string|min:3|max:255|regex:/^[\pL\s\-]+$/u',
             'email' => ['required', 'min:5', 'max:255', 'regex:/^[A-Za-z0-9\._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/i', 'unique:users', function ($attribute, $value, $fail) {
                 if ($value === strtoupper($value)) {
@@ -135,7 +149,7 @@ class UserController extends Controller
             'department' => 'required|integer|in:1,2,3,4,5,6',
             'position' => 'required|integer|in:1,2,3,4,5,6',
             'phone' => 'required|regex:/^\(\d{3}\) \d{3}-\d{4}$/',
-            'notes' => 'required|min:5|max:2147483647',
+            'notes' => 'required|min:3|max:2147483647',
             // 'role' => 'required|integer|exists:roles,id',
             'password' => 'required|min:5|max:25|confirmed',
             'password_confirmation' => 'required|min:5|max:25',
@@ -143,35 +157,41 @@ class UserController extends Controller
 
         try {
 
-            $userData = [
-                'name' => $request->name,
-                'email' => $request->email,
-                'department' => $request->department,
-                'position' => $request->position,
+            $user = User::create([
+                'uuid' => Str::uuid(),
+                'name' => $this->shared->clearString($validatedData['name']),
+                'email' => $validatedData['email'],
+                'department' => $validatedData['department'],
+                'position' => $validatedData['position'],
                 'is_active' => true,
-                'phone' => $request->phone,
-                'notes' => $request->notes,
+                'last_login' => null,
+                'phone' => $validatedData['phone'],
+                'notes' => $validatedData['notes'],
                 // 'role' => optional($request->roles->first())->name ?? 'N/A',
-            ];
+                'password' => Hash::make($validatedData['password'])
+            ]);
 
+            User::created($user);
 
-            User::created($userData);
-
-            return $this->shared->sendResponse($userData, 'Deatail user.');
+            return $this->shared->sendResponse($user, 'Usuario registrado.', Response::HTTP_CREATED);
         } catch (Exception $error) {
-            return $this->shared->sendError($error->getMessage());
+            return $this->shared->sendError($error->getMessage(), Response::HTTP_BAD_REQUEST);
         }
     }
 
-    public function deatailUser($uuid)
+    public function deatailUser(Request $request)
     {
         # Politica para saber si el usuario cuenta con los permisos deacuerdo a su rol asignado
         // Gate::authorize('deatailUser');
 
+        $validatedData = $request->validate([
+            'uuid' => 'required|string|uuid|exists:users,uuid',
+        ]);
+
         try {
             $user = User::with('roles')
                 ->select('users.*')
-                ->where('uuid', $uuid)
+                ->where('uuid', $validatedData['uuid'])
                 ->first();
 
             $userData = [
@@ -188,9 +208,9 @@ class UserController extends Controller
                 'created_at' => date('d-m-Y H:i:s', strtotime($user->created_at)),
             ];
 
-            return $this->shared->sendResponse($userData, 'Deatail user.');
+            return $this->shared->sendResponse($userData, 'Deatail user.', Response::HTTP_OK);
         } catch (Exception $error) {
-            return $this->shared->sendError($error->getMessage());
+            return $this->shared->sendError($error->getMessage(), Response::HTTP_BAD_REQUEST);
         }
     }
 
@@ -198,14 +218,19 @@ class UserController extends Controller
     {
         # Politica para saber si el usuario cuenta con los permisos deacuerdo a su rol asignado
         // Gate::authorize('deleteUser');
-        return $this->shared->sendResponse([], 'Usuario eliminado con exito.');
-        $this->validate($request, [
-            'uuid' => 'required|string|exists:users,uuid',
+        $validatedData = $request->validate([
+            'uuid' => 'required|string|uuid|exists:users,uuid',
         ]);
 
         try {
-            $user = User::where('uuid', $request->uuid)->first();
-            $user->delete();
+            /* 
+                NOTA: En estas acciones no se eliminara por completo el usuario, por temas de integridad de los datos se implementara
+                el auto borrado mediante periodos de tiempos, manteniendo el registro oculto de esta forma si se decea recuperar el 
+                registro simplemente eliminar la propiedad deleted_at
+            */
+            $user = User::where('uuid', $validatedData['uuid'])->first();
+            $user->deleted_at =  Carbon::now();
+            $user->save();
 
             return $this->shared->sendResponse($user, 'Usuario eliminado con exito.');
         } catch (Exception $error) {
--- routes/api.php
+++ routes/api.php
@@ -16,24 +16,24 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
-// Administracion cuenta
-Route::group(['prefix' => '/', 'middleware' => 'auth'], function () {
-    Route::get('/cuenta/{uuid}', [UserController::class, 'getAccount'])->name('getAccount');
-    Route::put('/cuenta/{uuid}', [UserController::class, 'editAccount'])->name('editAccount');
+# Administracion de empresa
+Route::prefix('administration')->middleware('auth:sanctum')->controller(UserController::class)->group(function () {
+    Route::post('/obtener-cuenta', 'getAccount');
+    Route::post('/editar-cuenta', 'editAccount');
 });
 
 // Administracion Usuarios
-Route::group(['prefix' => 'administracion', 'middleware' => 'auth'], function () {
-    Route::get('/usuarios', [UserController::class, 'listUser'])->name('listUser');
-    Route::get('/usuario/{uuid}', [UserController::class, 'deatailUser'])->name('deatailUser');
-    Route::post('/usuario/registrro', [UserController::class, 'createUser'])->name('createUser');
-    Route::post('/usuario', [UserController::class, 'deleteUser'])->name('deleteUser');
+Route::prefix('administration')->middleware('auth:sanctum')->controller(UserController::class)->group(function () {
+    Route::get('/usuarios', 'listUser');
+    Route::post('/obtener-usuario', 'deatailUser');
+    Route::post('/registro-usuario', 'createUser');
+    Route::post('/eliminar-usuario', 'deleteUser');
 });
